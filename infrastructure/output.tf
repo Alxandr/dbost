4ba8228351e066f5d02731f24ffd3465b7a93374
Aleksander Heintz <alxandr@alxandr.me>
2023-08-15 12:56:16 +0200

    dedupe outputs

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -3,7 +3,7 @@ output "database_address" {
   value       = aws_db_instance.dbost_db.address
 }
 
-output "database_address" {
+output "database_endpoint" {
   description = "The endpoint of the database"
   value       = aws_db_instance.dbost_db.endpoint
 }
