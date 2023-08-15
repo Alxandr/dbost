63704d0b7e11c764c88d7867c719a0d6d9b92c14
Aleksander Heintz <alxandr@alxandr.me>
2023-08-15 10:42:20 +0200

    Add postgresql inbound traffic rule

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,6 +41,22 @@ module "vpc" {
   single_nat_gateway = true
 }
 
+resource "aws_security_group" "rds" {
+  name_prefix = "${local.name}-rds"
+  description = "Allow PostgreSQL inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  tags = local.tags
+}
+
 resource "random_password" "db_master_password" {
   length           = 32
   special          = true
