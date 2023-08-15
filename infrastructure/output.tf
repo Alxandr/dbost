97aa42dc695bc93461827ddbf9daae335cbb9110
Aleksander Heintz <alxandr@alxandr.me>
2023-08-15 14:59:50 +0200

    output db password

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -7,3 +7,9 @@ output "database_endpoint" {
   description = "The endpoint of the database"
   value       = aws_db_instance.dbost_db.endpoint
 }
+
+output "database_password" {
+  description = "value of the database password"
+  value       = random_password.db_master_password.result
+  sensitive   = true
+}
