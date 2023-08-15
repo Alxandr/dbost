6c60bfb80613881884d8fc76b1742b989b7dd63b
Aleksander Heintz <alxandr@alxandr.me>
2023-08-16 01:20:19 +0200

    create and push images

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -2,3 +2,28 @@
 resource "aws_ecs_cluster" "cluster" {
   name = "dbost-cluster"
 }
+
+##### DB migrator task #####
+resource "aws_ecs_task_definition" "dbost-db-migrator" {
+  family                   = "dbost-db-migrator"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 0.5
+  memory                   = 1024
+  container_definitions = jsonencode([{
+    name   = "dbost-db-migrator"
+    image  = "ghcr.io/alxandr/dbost/migrator:latest"
+    cpu    = 1
+    memory = 1024
+    environment = [
+      {
+        name  = "DATABASE_URL"
+        value = "postgres://${postgresql_role.migrator.name}:${postgresql_role.migrator.password}@${aws_db_instance.dbost_db.endpoint}/postgres"
+      },
+      {
+        name  = "DATABASE_SCHEMA"
+        value = "public"
+      }
+    ]
+  }])
+}
