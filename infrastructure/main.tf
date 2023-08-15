0c77bdb662a8b4c9d5f8fab015c03435c955df42
Aleksander Heintz <alxandr@alxandr.me>
2023-08-15 10:32:14 +0200

    overfancy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,16 +6,37 @@ data "aws_availability_zones" "available" {
   }
 }
 
+locals {
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.1"
 
-  name = "dBost"
-  cidr = "10.0.0.0/16"
-  azs  = data.aws_availability_zones.available.names
+  name             = "dBost"
+  cidr             = "10.0.0.0/16"
+  azs              = local.azs
+  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 8)]
+  intra_subnets    = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 20)]
+  # elasticache_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 12)]
+  # redshift_subnets    = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 16)]
   # public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   # enable_dns_hostnames = true
   # enable_dns_support   = true
+
+  create_database_subnet_group  = false
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
 resource "random_password" "db_master_password" {
