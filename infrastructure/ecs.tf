992796c633bd47a48f2a5217339f7e9dfc4bee9c
Aleksander Heintz <alxandr@alxandr.me>
2023-08-16 01:25:32 +0200

    Up the cpuage

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_task_definition" "dbost-db-migrator" {
   family                   = "dbost-db-migrator"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 0.5
+  cpu                      = 1
   memory                   = 1024
   container_definitions = jsonencode([{
     name   = "dbost-db-migrator"
