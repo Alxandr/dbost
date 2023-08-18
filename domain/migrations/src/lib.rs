2545e747ca3a2d3b0397c0d1e883906bff26512f
Aleksander Heintz <alxandr@alxandr.me>
2023-08-18 15:49:36 +0200

    descriptions

--- domain/migrations/src/lib.rs
+++ domain/migrations/src/lib.rs
@@ -21,15 +21,15 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
 	fn migrations() -> Vec<Box<dyn MigrationTrait>> {
 		vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20230730_233755_create_theme_song_entity::Migration),
-            Box::new(m20230801_120415_gen_random_uuid::Migration),
-            Box::new(m20230802_075725_search_indices::Migration),
-            Box::new(m20230807_142613_versioning::Migration),
-            Box::new(m20230811_170956_sessions::Migration),
-            Box::new(m20230813_063316_artwork::Migration),
-            Box::new(m20230813_131452_hot_indices::Migration),
-            Box::new(m20230818_124952_descriptions::Migration),
-        ]
+			Box::new(m20220101_000001_create_table::Migration),
+			Box::new(m20230730_233755_create_theme_song_entity::Migration),
+			Box::new(m20230801_120415_gen_random_uuid::Migration),
+			Box::new(m20230802_075725_search_indices::Migration),
+			Box::new(m20230807_142613_versioning::Migration),
+			Box::new(m20230811_170956_sessions::Migration),
+			Box::new(m20230813_063316_artwork::Migration),
+			Box::new(m20230813_131452_hot_indices::Migration),
+			Box::new(m20230818_124952_descriptions::Migration),
+		]
 	}
 }
